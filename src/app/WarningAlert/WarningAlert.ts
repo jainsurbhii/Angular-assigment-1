0b23ae287c404c1874d80378f658d05accba10fd
surbhijain <surbhijain.jain4@gmail.com>
2022-07-25 19:49:18 +0530

    file added

---
+++ src/app/WarningAlert/WarningAlert.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+@Component({
+  selector:'app-server',
+  templateUrl:'./WarningAlert.html',
+  styleUrls:['./WarningAlert.css']
+})
+export class WarningAlert{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {WarningAlert} from "./WarningAlert/WarningAlert";
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlert,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
